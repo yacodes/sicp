cdc24cd05e5aeff92ae1b1f108126e6cca6c76a0
Aleksandr Yakunichev <hi@ya.codes>
2022-01-18 22:03:00 +0300

    Update 01-building-abstractions-with-procedures.scm

--- 01-building-abstractions-with-procedures.scm
+++ 01-building-abstractions-with-procedures.scm
@@ -513,3 +513,194 @@
 ;; https://codology.net/post/sicp-solution-exercise-1-15/
 ;; The order of growth in number of steps is Θ(log(a)).
 ;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.16: Design a procedure that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt. (Hint: Using the observation that (b^(n/2))^2 = (b^2)^(n/2), keep, along with the exponent n and the base b, an additional state variable a, and define the state transformation in such a way that the product a b n is unchanged from state to state. At the beginning of the process a is taken to be 1, and the answer is given by the value of a at the end of the process. In general, the technique of defining an invariant quantity that remains unchanged from state to state is a powerful way to think about the design of iterative algorithms.) 
+
+(define (fast-expt-iter a b n)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter a (* b b) (/ n 2)))
+        (else (fast-expt-iter (* a b) b (- n 1)))))
+
+(fast-expt-iter 1 9 7)
+;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.17: The exponentiation algorithms in this section are based on performing exponentiation by means of repeated multiplication. In a similar way, one can perform integer multiplication by means of repeated addition. The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the expt procedure:
+
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+;; This algorithm takes a number of steps that is linear in b. Now suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. Using these, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps. 
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (double x)
+    (+ x x))
+
+(define (halve x)
+    (/ x 2))
+
+(define (fast-mult a b)
+  (cond ((= b 0)
+         0)
+        ((even? b)
+         (fast-mult (double a) (halve b)))
+        (else
+         (+ a (fast-mult a (- b 1))))))
+
+(fast-mult 17 19)
+,trace (fast-mult 17 19)
+;; trace: |  (fast-mult 17 19)
+;; trace: |  |  (fast-mult 17 18)
+;; trace: |  |  (fast-mult 34 9)
+;; trace: |  |  |  (fast-mult 34 8)
+;; trace: |  |  |  (fast-mult 68 4)
+;; trace: |  |  |  (fast-mult 136 2)
+;; trace: |  |  |  (fast-mult 272 1)
+;; trace: |  |  |  |  (fast-mult 272 0)
+;; trace: |  |  |  |  0
+;; trace: |  |  |  272
+;; trace: |  |  306
+;; trace: |  323
+
+;; https://codology.net/post/sicp-solution-exercise-1-17/
+;; --------------------------------
+
+
+;; --------------------------------
+;; Other exercises from chapter 1.2 are skipped. Check codology.net for the answers.
+;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.29: Simpson’s Rule is a more accurate method of numerical integration than the method illustrated above. Using Simpson’s Rule, the integral of a function f between a and b is approximated as 
+;; h/3 ⋅ (y₀ + 4y₁ + 2y₂ + 4y₃ + 2y₄ + ⋯ + 2y₍ₙ₋₂₎ + 4y₍ₙ₋₁₎ + yₙ)
+;; where h = ( b − a ) / n , for some even integer n , and y k = f ( a + k h ) . (Increasing n increases the accuracy of the approximation.) Define a procedure that takes as arguments f , a , b , and n and returns the value of the integral, computed using Simpson’s Rule. Use your procedure to integrate cube between 0 and 1 (with n = 100 and n = 1000 ), and compare the results to those of the integral procedure shown above. 
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+; assuming n is even
+(define (integral-simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (add-2h x) (+ x h h))
+  (* (+ (f a)
+        (* 2 (sum f a       add-2h b))
+        (* 4 (sum f (+ a h) add-2h b))
+        (f b))
+     (/ h 3)))
+
+(define (cube x) (* x x x))
+
+(integral cube 0 1 0.01)
+(integral cube 0 1 0.001)
+(integral-simpson cube 0 1.0 100)
+(integral-simpson cube 0 1.0 1000)
+
+;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.30: The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition: 
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (inc n) (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.31: 1. The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures. Write an analogous procedure called product that returns the product of the values of a function at points over a given range. Show how to define factorial in terms of product. Also use product to compute approximations to π using the formula 
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (factorial n)
+  (product identity 1 1+ n))
+
+(factorial 4)
+
+(define (wallis-product n)
+  (define (term n)
+    (* (/ (* 2 n)
+          (- (* 2 n) 1))
+       (/ (* 2 n)
+          (+ (* 2 n) 1))))
+  (product term 1.0 inc n))
+
+(wallis-product 0)
+
+;; If your product procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+
+;; Iterative product procedure
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+;; --------------------------------
+
+
+;; --------------------------------
+;; Exercise 1.32:
+;; 1. Show that sum and product (Exercise 1.31) are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function: 
+(accumulate 
+ combiner null-value term a next b)
+;; Accumulate takes as arguments the same term and range specifications as sum and product, together with a combiner procedure (of two arguments) that specifies how the current term is to be combined with the accumulation of the preceding terms and a null-value that specifies what base value to use when the terms run out. Write accumulate and show how sum and product can both be defined as simple calls to accumulate. 
+
+;; Iterative version
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+;; 2. If your accumulate procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process. 
+
+;; Recursive version
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+         (accumulate combiner null-value term (next a) next b))))
+
+;; Example of usage
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(sum + 0 1+ 120) ;; Simple summing
+;; --------------------------------
